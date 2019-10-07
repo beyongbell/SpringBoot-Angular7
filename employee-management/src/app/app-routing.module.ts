68c8164334eece3e23926077b38a8a63727c4a31
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-07 16:53:50 +0700

    Commit Employee Component

--- employee-management/src/app/app-routing.module.ts
+++ employee-management/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- employee-management/src/app/app.module.ts
+++ employee-management/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
