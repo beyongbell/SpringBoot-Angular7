d287bd848420d9b5e31e0444da866f0be98892e9
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-10-07 16:22:26 +0700

    First Commit

---
+++ springboot-application-angular7/src/main/java/com/springbootapplicationangular7/project/SpringbootApplicationAngular7Application.java
@@ -0,0 +1,13 @@
+package com.springbootapplicationangular7.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationAngular7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplicationAngular7Application.class, args);
+	}
+
+}
